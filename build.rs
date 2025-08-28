8a925f2bd7bc935f106f587e7711196a0ae1cb25
Mohammed Alyousef <may642_2000@hotmail.com>
2025-08-29 01:12:25 +0300

    add rustc-check-cfg for showconsole

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-env-changed=SHOWCONSOLE");
+    println!("cargo::rustc-check-cfg=cfg(showconsole)");
     if let Ok(v) = std::env::var("SHOWCONSOLE") {
         if v == "1" {
             println!("cargo:rustc-cfg=showconsole");
